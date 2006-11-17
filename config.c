ee8bda703e747de593adc5e12b611834b842cca5
Juliusz Chroboczek <jch@pps.jussieu.fr>
2006-11-16 17:54:54 -0800

    Remove useless includes from config.c.
    darcs-hash:20061117015454-4cc09-c5bcd68b5b8a7792844775b0bf612774de4112b0.gz

--- config.c
+++ config.c
@@ -20,10 +20,6 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 */
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <errno.h>
 #include "polipo.h"
 
 ConfigVariablePtr configVariables = NULL;
