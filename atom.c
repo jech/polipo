3aef54dad9e055bdea386c04baf6de45b6c04396
Juliusz Chroboczek <jch@pps.jussieu.fr>
2006-11-18 18:56:40 -0800

    Allow internAtomError to take a NULL format.
    darcs-hash:20061119025640-4cc09-e662dbf467b2eb54aa32682c90396bba3075824a.gz

--- atom.c
+++ atom.c
@@ -230,20 +230,27 @@ internAtomErrorV(int e, const char *f, va_list args)
     char *s1, *s2;
     int n, rc;
 
-    s1 = vsprintf_a(f, args);
-    if(s1 == NULL)
-        return NULL;
-    n = strlen(s1);
+    if(f) {
+        s1 = vsprintf_a(f, args);
+        if(s1 == NULL)
+            return NULL;
+        n = strlen(s1);
+    } else {
+        s1 = NULL;
+        n = 0;
+    }
 
     s2 = malloc(n + 70);
     if(s2 == NULL) {
         free(s1);
         return NULL;
     }
-    strcpy(s2, s1);
-    free(s1);
+    if(s1) {
+        strcpy(s2, s1);
+        free(s1);
+    }
 
-    rc = snprintf(s2 + n, 69, ": %s", es);
+    rc = snprintf(s2 + n, 69, f ? ": %s" : "%s", es);
     if(rc < 0 || rc >= 69) {
         free(s2);
         return NULL;
