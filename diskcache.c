10f2e81d26a05c90a6d39449fd55a232be421794
Juliusz Chroboczek <jch@pps.jussieu.fr>
2006-11-26 13:27:20 -0800

    More tweaks to chooseBodyOffset.
    darcs-hash:20061126212720-4cc09-f7943361270317cd18d5e8557ed7c635fea9f8b9.gz

--- diskcache.c
+++ diskcache.c
@@ -507,7 +507,7 @@ chooseBodyOffset(int n, ObjectPtr object)
     int length = MAX(object->size, object->length);
     int body_offset;
 
-    if(object->length >= 0 && object->length + n < 4096 - 50)
+    if(object->length >= 0 && object->length + n < 4096 - 4)
         return -1;              /* no gap for small objects */
 
     if(n <= 128)
@@ -524,23 +524,33 @@ chooseBodyOffset(int n, ObjectPtr object)
         body_offset = 2048;
     else if(n < 2048)
         body_offset = 4096;
-    else body_offset = ((n + 4095) / 4096 + 1) * 4096;
+    else
+        body_offset = ((n + 32 + 4095) / 4096 + 1) * 4096;
 
     /* Tweak the gap so that we don't use up a full disk block for
        a small tail */
     if(object->length >= 0 && object->length < 64 * 1024) {
         int last = (body_offset + object->length) % 4096;
-        if(last < body_offset / 4 && n < body_offset - last - 50)
+        int gap = body_offset - n - 32;
+        if(last < gap / 2)
             body_offset -= last;
     }
 
-    /* Rewriting large objects is expensive -- don't use small gaps */
-    if(length >= 64 * 1024)
-        body_offset = MAX(body_offset, 1024);
-    if(length >= 256 * 1024)
-        body_offset = MAX(body_offset, 2048);
-    if(length >= 512 * 1024)
-        body_offset = MAX(body_offset, 4096);
+    /* Rewriting large objects is expensive -- don't use small gaps.
+       This has the additional benefit of block-aligning large bodies. */
+    if(length >= 64 * 1024) {
+        int min_gap, min_offset;
+        if(length >= 512 * 1024)
+            min_gap = 4096;
+        else if(length >= 256 * 1024)
+            min_gap = 2048;
+        else
+            min_gap = 1024;
+
+        min_offset = ((n + 32 + min_gap - 1) / min_gap + 1) * min_gap;
+        body_offset = MAX(body_offset, min_offset);
+    }
+
     return body_offset;
 }
  
