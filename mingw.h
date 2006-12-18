cbe5b0f5bc37310c9d05079b22533a59fffceafe
Juliusz Chroboczek <jch@pps.jussieu.fr>
2006-12-18 14:51:16 -0800

    Reindent mingw.h.
    darcs-hash:20061218225116-4cc09-aac3dbd93851fb811b655a8086e134719da3b56d.gz

--- mingw.h
+++ mingw.h
@@ -34,92 +34,91 @@ THE SOFTWARE.
 
 #ifdef MINGW
 
-  /* Unfortunately, there's no hiding it. */
-  #define HAVE_WINSOCK 1
+/* Unfortunately, there's no hiding it. */
+#define HAVE_WINSOCK 1
 
-  /* At time of writing, a fair bit of stuff doesn't work under Mingw.
-   * Hopefully they will be fixed later (especially the disk-cache).
-   */
-  #define NO_IPv6 1
+/* At time of writing, a fair bit of stuff doesn't work under Mingw.
+ * Hopefully they will be fixed later (especially the disk-cache).
+ */
+#define NO_IPv6 1
 
-  #include <io.h>
+#include <io.h>
 
-  #define S_IROTH S_IREAD
+#define S_IROTH S_IREAD
 
-  /* Pull in winsock.h for (almost) berkeley sockets. */
-  #include <winsock.h>
-  #define ENOTCONN        WSAENOTCONN
-  #define EWOULDBLOCK     WSAEWOULDBLOCK
-  #define ENOBUFS         WSAENOBUFS
-  #define ECONNRESET      WSAECONNRESET
-  #define ESHUTDOWN       WSAESHUTDOWN
-  #define EAFNOSUPPORT    WSAEAFNOSUPPORT
-  #define EPROTONOSUPPORT WSAEPROTONOSUPPORT
-  #define EINPROGRESS     WSAEINPROGRESS
-  #define EISCONN         WSAEISCONN
+/* Pull in winsock.h for (almost) berkeley sockets. */
+#include <winsock.h>
+#define ENOTCONN        WSAENOTCONN
+#define EWOULDBLOCK     WSAEWOULDBLOCK
+#define ENOBUFS         WSAENOBUFS
+#define ECONNRESET      WSAECONNRESET
+#define ESHUTDOWN       WSAESHUTDOWN
+#define EAFNOSUPPORT    WSAEAFNOSUPPORT
+#define EPROTONOSUPPORT WSAEPROTONOSUPPORT
+#define EINPROGRESS     WSAEINPROGRESS
+#define EISCONN         WSAEISCONN
 
-  /* winsock doesn't feature poll(), so there is a version implemented
-   * in terms of select() in mingw.c. The following definitions
-   * are copied from linux man pages. A poll() macro is defined to
-   * call the version in mingw.c.
-   */
-  #define POLLIN      0x0001    /* There is data to read */
-  #define POLLPRI     0x0002    /* There is urgent data to read */
-  #define POLLOUT     0x0004    /* Writing now will not block */
-  #define POLLERR     0x0008    /* Error condition */
-  #define POLLHUP     0x0010    /* Hung up */
-  #define POLLNVAL    0x0020    /* Invalid request: fd not open */
-  struct pollfd {
-       SOCKET fd;        /* file descriptor */
-       short events;     /* requested events */
-       short revents;    /* returned events */
-  };
-  #define poll(x, y, z)        mingw_poll(x, y, z)
+/* winsock doesn't feature poll(), so there is a version implemented
+ * in terms of select() in mingw.c. The following definitions
+ * are copied from linux man pages. A poll() macro is defined to
+ * call the version in mingw.c.
+ */
+#define POLLIN      0x0001    /* There is data to read */
+#define POLLPRI     0x0002    /* There is urgent data to read */
+#define POLLOUT     0x0004    /* Writing now will not block */
+#define POLLERR     0x0008    /* Error condition */
+#define POLLHUP     0x0010    /* Hung up */
+#define POLLNVAL    0x0020    /* Invalid request: fd not open */
+struct pollfd {
+    SOCKET fd;        /* file descriptor */
+    short events;     /* requested events */
+    short revents;    /* returned events */
+};
+#define poll(x, y, z)        mingw_poll(x, y, z)
 
-  /* These wrappers do nothing special except set the global errno variable if
-   * an error occurs (winsock doesn't do this by default). They set errno
-   * to unix-like values (i.e. WSAEWOULDBLOCK is mapped to EAGAIN), so code
-   * outside of this file "shouldn't" have to worry about winsock specific error
-   * handling. 
-   */
-  #define socket(x, y, z)      mingw_socket(x, y, z)
-  #define connect(x, y, z)     mingw_connect(x, y, z)
-  #define accept(x, y, z)      mingw_accept(x, y, z)
-  #define shutdown(x, y)       mingw_shutdown(x, y)
-  #define getpeername(x, y, z) mingw_getpeername(x, y, z)
+/* These wrappers do nothing special except set the global errno variable if
+ * an error occurs (winsock doesn't do this by default). They set errno
+ * to unix-like values (i.e. WSAEWOULDBLOCK is mapped to EAGAIN), so code
+ * outside of this file "shouldn't" have to worry about winsock specific error
+ * handling.
+ */
+#define socket(x, y, z)      mingw_socket(x, y, z)
+#define connect(x, y, z)     mingw_connect(x, y, z)
+#define accept(x, y, z)      mingw_accept(x, y, z)
+#define shutdown(x, y)       mingw_shutdown(x, y)
+#define getpeername(x, y, z) mingw_getpeername(x, y, z)
 
-  /* Wrapper macros to call misc. functions mingw is missing */
-  #define sleep(x)             mingw_sleep(x)
-  #define inet_aton(x, y)      mingw_inet_aton(x, y)
-  #define gettimeofday(x, y)   mingw_gettimeofday(x, y)
+/* Wrapper macros to call misc. functions mingw is missing */
+#define sleep(x)             mingw_sleep(x)
+#define inet_aton(x, y)      mingw_inet_aton(x, y)
+#define gettimeofday(x, y)   mingw_gettimeofday(x, y)
 
-  #define mkdir(x, y) mkdir(x)
+#define mkdir(x, y) mkdir(x)
 
-  /* Winsock uses int instead of the usual socklen_t */
-  typedef int socklen_t;
+/* Winsock uses int instead of the usual socklen_t */
+typedef int socklen_t;
 
-  /* Function prototypes for functions in mingw.c */
-  unsigned int mingw_sleep(unsigned int);
-  int     mingw_inet_aton(const char *, struct in_addr *);
-  int     mingw_gettimeofday(struct timeval *, char *);
-  int     mingw_poll(struct pollfd *, unsigned int, int);
-  SOCKET  mingw_socket(int, int, int);
-  int     mingw_connect(SOCKET, struct sockaddr*, socklen_t);
-  SOCKET  mingw_accept(SOCKET, struct sockaddr*, socklen_t *);
-  int     mingw_shutdown(SOCKET, int);
-  int     mingw_getpeername(SOCKET, struct sockaddr*, socklen_t *);
+/* Function prototypes for functions in mingw.c */
+unsigned int mingw_sleep(unsigned int);
+int     mingw_inet_aton(const char *, struct in_addr *);
+int     mingw_gettimeofday(struct timeval *, char *);
+int     mingw_poll(struct pollfd *, unsigned int, int);
+SOCKET  mingw_socket(int, int, int);
+int     mingw_connect(SOCKET, struct sockaddr*, socklen_t);
+SOCKET  mingw_accept(SOCKET, struct sockaddr*, socklen_t *);
+int     mingw_shutdown(SOCKET, int);
+int     mingw_getpeername(SOCKET, struct sockaddr*, socklen_t *);
 
-  /* Three socket specific macros */
-  #define READ(x, y, z)  mingw_read_socket(x, y, z)
-  #define WRITE(x, y, z) mingw_write_socket(x, y, z)
-  #define CLOSE(x)       mingw_close_socket(x)
+/* Three socket specific macros */
+#define READ(x, y, z)  mingw_read_socket(x, y, z)
+#define WRITE(x, y, z) mingw_write_socket(x, y, z)
+#define CLOSE(x)       mingw_close_socket(x)
 
-  int mingw_read_socket(SOCKET, void *, int);
-  int mingw_write_socket(SOCKET, void *, int);
-  int mingw_close_socket(SOCKET);
+int mingw_read_socket(SOCKET, void *, int);
+int mingw_write_socket(SOCKET, void *, int);
+int mingw_close_socket(SOCKET);
 
-  int mingw_setnonblocking(SOCKET, int);
-
+int mingw_setnonblocking(SOCKET, int);
 #endif
 
 #ifndef HAVE_READV_WRITEV
