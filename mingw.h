7445272626a715bc50c02865cfd491d142841057
Juliusz Chroboczek <jch@pps.jussieu.fr>
2006-12-18 14:50:33 -0800

    Add my copyright to the mingw files.
    darcs-hash:20061218225033-4cc09-2dc171694129c75ceb3f2a9b989ed145b3ee45eb.gz

--- mingw.h
+++ mingw.h
@@ -1,5 +1,6 @@
 /*
 Copyright (c) 2006 by Dan Kennedy.
+Copyright (c) 2006 by Juliusz Chroboczek.
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
