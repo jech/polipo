dc9784904772c0a58b47c4b3de8b81cf49102078
Juliusz Chroboczek <jch@pps.jussieu.fr>
2008-09-07 17:11:25 -0700

    Redirect 0, 1 and 2 to /dev/null when daemonising.
    darcs-hash:20080908001125-4cc09-2cf154e909dc08d16be6d28774f0a643b53405b9.gz

--- util.c
+++ util.c
@@ -534,9 +534,27 @@ do_daemonise(int noclose)
         exit(0);
 
     if(!noclose) {
+        int fd;
         close(0);
         close(1);
         close(2);
+        /* Leaving the default file descriptors free is not a good
+           idea, as it will cause library functions such as abort to
+           thrash the on-disk cache. */
+        fd = open("/dev/null", O_RDONLY);
+        if(fd > 0) {
+            dup2(fd, 0);
+            close(fd);
+        }
+        fd = open("/dev/null", O_WRONLY);
+        if(fd >= 0) {
+            if(fd != 1)
+                dup2(fd, 1);
+            if(fd != 2)
+                dup2(fd, 2);
+            if(fd != 1 && fd != 2)
+                close(fd);
+        }
     }
     rc = setsid();
     if(rc < 0) {
