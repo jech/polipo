73c8b5d86726848ff4feac16c68d648bf0c0f42f
Juliusz Chroboczek <jch@pps.jussieu.fr>
2009-04-27 13:01:52 -0700

    Avoid a memory leak in discardServer.
    Ignore-this: 32c558d54787010a83d504231e9f3879
    Thanks to Ming Fu.
    
    darcs-hash:20090427200152-4cc09-e7b002c90da52db1ad504a1ebb5c65fdeef68563.gz

--- server.c
+++ server.c
@@ -117,6 +117,13 @@ discardServer(HTTPServerPtr server)
         previous->next = server->next;
     }
 
+    if(server->connection)
+        free(server->connection);
+    if(server->idleHandler)
+        free(server->idleHandler);
+    if(server->name)
+        free(server->name);
+
     free(server);
 }
 
