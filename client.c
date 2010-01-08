4ac15d486446d67d6629d7afac028975bb97da3f
Christopher Davis <chrisd@mangrin.org>
2010-01-08 15:44:48 -0800

    Prevent an infinite loop when a bodyless 204 or 1xx response is encountered.
    Hopefully, this should fix bug #1185.
    (cherry picked from commit eb91a8710f17870e4151f4889006933be8815dea)

--- client.c
+++ client.c
@@ -1162,7 +1162,9 @@ httpClientNoticeRequest(HTTPRequestPtr request, int novalidate)
         request->to = -1;
     }
 
-    if(request->method == METHOD_HEAD)
+    if(request->method == METHOD_HEAD ||
+       request->object->code == 204 ||
+       request->object->code < 200)
         haveData = !(request->object->flags & OBJECT_INITIAL);
     else
         haveData = 
@@ -2122,7 +2124,9 @@ httpServeObjectStreamHandlerCommon(int kind, int status,
         return 1;
     }
 
-    if(connection->request->method == METHOD_HEAD ||
+    if(request->method == METHOD_HEAD ||
+       request->object->code == 204 ||
+       request->object->code < 200 ||
        condition_result == CONDITION_NOT_MODIFIED) {
         httpClientFinish(connection, 0);
         return 1;
