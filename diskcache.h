2fe49ea7851b388679a9cc0e5cf8ff3b3118cc1f
Juliusz Chroboczek <jch@pps.jussieu.fr>
2010-01-29 01:18:01 +0100

    Remove support for non-writable disk cache entries.
    This removes the support for opening disk cache entries O_RDONLY,
    which as far as I am aware nobody has ever used.

--- diskcache.h
+++ diskcache.h
@@ -1,5 +1,5 @@
 /*
-Copyright (c) 2003-2006 by Juliusz Chroboczek
+Copyright (c) 2003-2010 by Juliusz Chroboczek
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
@@ -33,7 +33,6 @@ typedef struct _DiskCacheEntry {
     off_t size;
     int body_offset;
     short local;
-    short writeable;
     short metadataDirty;
     struct _DiskCacheEntry *next;
     struct _DiskCacheEntry *previous;
