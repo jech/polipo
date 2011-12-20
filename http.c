7956b59e552e313229da3060e0f4e793dec3f06e
William Manley <william.manley@youview.com>
2011-12-20 21:01:51 +0000

    Fix typo in httpTweakCachability
    An object flag was being ORed with some cache-control flags.  This is
    clearly the wrong thing to do.  I've fixed this by ORing the object
    flag with the object flags of the object which seems more correct,
    however I'm unsure of the full implications of the patch.

--- http.c
+++ http.c
@@ -1009,8 +1009,10 @@ httpTweakCachability(ObjectPtr object)
     int code = object->code;
 
     if((object->cache_control & CACHE_AUTHORIZATION) &&
-       !(object->cache_control & CACHE_PUBLIC))
-        object->cache_control |= (CACHE_NO_HIDDEN | OBJECT_LINEAR);
+       !(object->cache_control & CACHE_PUBLIC)) {
+        object->cache_control |= CACHE_NO_HIDDEN;
+        object->flags |= OBJECT_LINEAR;
+    }
 
     /* This is not required by RFC 2616 -- but see RFC 3143 2.1.1.  We
        manically avoid caching replies that we don't know how to
@@ -1021,8 +1023,8 @@ httpTweakCachability(ObjectPtr object)
        code != 300 && code != 301 && code != 302 && code != 303 &&
        code != 304 && code != 307 &&
        code != 403 && code != 404 && code != 405 && code != 416) {
-        object->cache_control |=
-            (CACHE_NO_HIDDEN | CACHE_MISMATCH | OBJECT_LINEAR);
+        object->cache_control |= (CACHE_NO_HIDDEN | CACHE_MISMATCH);
+        object->flags |= OBJECT_LINEAR;
     } else if(code != 200 && code != 206 &&
               code != 300 && code != 301 && code != 304 &&
               code != 410) {
