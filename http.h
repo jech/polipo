ec6d1115248f251918a7c57595ac38d36a9e7fb4
Juliusz Chroboczek <jch@pps.jussieu.fr>
2014-01-06 04:27:40 +0100

    Add comments for request flags.

--- http.h
+++ http.h
@@ -50,10 +50,15 @@ typedef struct _HTTPRequest {
 } HTTPRequestRec, *HTTPRequestPtr;
 
 /* request->flags */
+/* If not present, drop the connection after this request. */
 #define REQUEST_PERSISTENT 1
+/* This client-side request has already been requested on the server-side. */
 #define REQUEST_REQUESTED 2
+/* This request is waiting for continue from the server. */
 #define REQUEST_WAIT_CONTINUE 4
+/* Force an error unconditionally -- used for client auth failures. */
 #define REQUEST_FORCE_ERROR 8
+/* This server-side request was pipelined. */
 #define REQUEST_PIPELINED 16
 
 typedef struct _HTTPConnection {
